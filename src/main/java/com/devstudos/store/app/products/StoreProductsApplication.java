e71d03eaac0053e78df605e2091d8dfcc223be95
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-28 09:23:25 -0600

    Registrando cliente en eureka

--- src/main/java/com/devstudos/store/app/products/StoreProductsApplication.java
+++ src/main/java/com/devstudos/store/app/products/StoreProductsApplication.java
@@ -1,9 +1,12 @@
-package com.devstudos.store.app.products.store_products;
+package com.devstudos.store.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StoreProductsApplication {
 
 	public static void main(String[] args) {

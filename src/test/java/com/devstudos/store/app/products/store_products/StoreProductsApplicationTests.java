c11b1cfc140c0650f2345d3380d95c4535d637fb
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-28 08:55:53 -0600

    First commit

---
+++ src/test/java/com/devstudos/store/app/products/store_products/StoreProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudos.store.app.products.store_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
